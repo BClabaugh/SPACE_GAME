b3772c811692c0a820799e8affd2db3e7bf5e786
BClabaugh <clabaughbla@gmail.com>
2023-11-11 17:19:11 -0600

    Update SpaceWebScraper.py
    i done

--- SpaceWebScraper.py
+++ SpaceWebScraper.py
@@ -1,2 +1,22 @@
-class main():
-    print("Hello")
+
+import re
+from bs4 import BeautifulSoup
+import requests
+
+def main():
+    website_scraped = requests.get("https://starlust.org/how-far-away-is-earth-from-the-sun/")
+    soup = BeautifulSoup(website_scraped.text, "html.parser")
+    data = soup.find("div", attrs={"class":"elementor-shortcode"})
+    new_data = list(map(int, re.findall('\d+', str(data))))
+    num = max_num_in_list(new_data)
+    print(round(num,-3))
+
+
+def max_num_in_list(list):
+    max = list[0]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+
+main()
